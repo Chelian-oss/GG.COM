59a8ea84eca105a5a539df86da8f6641e1348a1c
Chelian-oss <25093840@imail.sunway.edu.my>
2025-07-21 06:01:23 +0000

    gg.com

--- Part2/ex10.py
+++ Part2/ex10.py
@@ -1,9 +1,9 @@
 # TODO:
 # Create a function called say_hi that takes one argument (name)
-# It should display a string like: "Greetings Brother Oreo!" or "Greetings Sanz!"
+# It should display a string like: "Greetings Brother Oreo08" or "Greetings Sanz08"
 
 def say_hi(name):
-    print("Greetings", name + "!")
+    print("Greetings", name + "08")
 
 # Function calls
 say_hi("Brother Oreo")
