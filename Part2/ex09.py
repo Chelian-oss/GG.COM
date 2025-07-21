27b6757a119efb0d5b011c8e87e6833435d3ffec
Chelian-oss <25093840@imail.sunway.edu.my>
2025-07-21 05:56:39 +0000

    gg

---
+++ Part2/ex09.py
@@ -0,0 +1,9 @@
+# TODO: Create a function called my_function
+# that prints "Waddup brother" when called
+
+def my_function ():
+    print("Waddup brother")
+
+# Call the function
+my_function ()
+
---
+++ Part2/ex10.py
@@ -0,0 +1,10 @@
+# TODO:
+# Create a function called say_hi that takes one argument (name)
+# It should display a string like: "Greetings Brother Oreo!" or "Greetings Sanz!"
+
+def say_hi(name):
+    print("Greetings", name + "!")
+
+# Function calls
+say_hi("Brother Oreo")
+say_hi("Sanz")
